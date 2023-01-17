7fd9f3f0bdd5e2d2de424c09099da2854fdc5e94
chienvn <chiennguyen1702@gmail.com>
2023-01-17 22:06:45 +0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Route path="/" exact={true}>
+        <Dashboard></Dashboard>
+      </Route>
+      
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
   );
 }
 
