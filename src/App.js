44c861cf4330f69ffbcb1cfdaf16ed760b3573c8
chienvn <chiennguyen1702@gmail.com>
2023-01-17 22:12:36 +0700

    react router error page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <Route path="/" exact={true}>
-        <Dashboard></Dashboard>
-      </Route>
-      
-      <Route path="/login">
-        <Login />
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        
+        <Route path="/login">
+          <Login />
+        </Route>
+        
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 }
