78f90b8b62f6ce8e3eefa3fcd7d1abdd990e6ff9
nguyen.van.chien <mhand.chien@gmail.com>
2023-02-21 13:45:25 +0700

    auth0

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// dev-u24j2zcigjxek1wp.us.auth0.com
+// Ofl7ktnJnrXCrBNIusMdHMueiV5suoB0
+/*
+{
+  "sub": "google-oauth2|105020491603970362201",
+  "given_name": "Chiến",
+  "family_name": "Nguyễn",
+  "nickname": "chiennguyen1702",
+  "name": "Chiến Nguyễn",
+  "picture": "https://lh3.googleusercontent.com/a/AEdFTp7Q0m6ZIU97fzlsYI0EVMbhdqeBr1-EmOSTO_D1Lw=s96-c",
+  "locale": "vi",
+  "updated_at": "2023-02-21T06:42:56.000Z"
+}
+*/
 root.render(
   <React.StrictMode>
     <GithubProvider>
